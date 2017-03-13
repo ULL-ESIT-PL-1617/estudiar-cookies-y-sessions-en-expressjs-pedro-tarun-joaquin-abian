1f58c143fe930b66f962984a69c3565b5baeabae
Tarun Mohandas <alu0100891782@ull.edu.es>
2017-03-13 17:38:06 +0000

    Modificado hello-cookie

--- src/hello-cookie.js
+++ src/hello-cookie.js
@@ -1,37 +1,49 @@
-// hello-cookie.js
-// https://www.codementor.io/noddy/cookie-management-in-express-js-du107rmna
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var util = require('util');
+var express = require('express'),
+    app = express(),
+    session = require('express-session');
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
+var path = require('path')
 
-var app = express();
-app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/../img'))) 
+// Authentication and Authorization Middleware
+var auth = function(req, res, next) {
+  if (req.session && req.session.user === "who" && req.session.admin)
+    return next();
+  else
+    return res.sendStatus(401);
+};
 
-app.get('/cookie',function(req, res){
-     res.cookie('cookie_name', 'cookie_value', 
-       {expire: new Date() + 9999}).send(
-       "Cookie is set: goto to browser's console and write document.cookie.");
-});
-
-app.get('/', function(req, res) {
-  res.send("visit localhost:8080/cookie")
-});
-
-app.get('/show', function(req, res) {
-  console.log("Cookies :  "+util.inspect(req.cookies));
-  res.send("Cookies :  "+util.inspect(req.cookies));
-});
-
-app.get('/clearcookie', function(req,res){
-     res.clearCookie('cookie_name');
-     res.send('Cookie deleted');
-});
-
-var server = app.listen(8080, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+app.get('/', function(req, res, next){
+   res.send("Visit /content. If you cannot see anything, you should login at /login.To logout go to /logout")
 })
+ 
+// Login endpoint
+app.get('/login', function (req, res) {
+  if (!req.query.username || !req.query.password) {
+    res.send('login failed' + 'You should visit /login?username=user&password=yourpassword' + "\n Try user = who and password = whospassword");    
+  } else if(req.query.username === "who" || req.query.password === "whospassword") {
+    req.session.user = "who";
+    req.session.admin = true;
+    res.send("login success!");
+  }
+});
+ 
+// Logout endpoint
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.send("logout success!");
+});
+ 
+// Get content endpoint
+app.get('/content', auth, function (req, res) {
+    //res.sendFile('../kitten.jpg')
+    //res.write("You can only see this after you've logged in.");
+    res.send('<a>You can only see this after you have logged in.</a><br><img src="kitten.jpg">')
+});
+ 
+app.listen(3000);
+console.log("app running at http://localhost:3000");
